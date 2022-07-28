4549c6a8790188e4e1fc4e1e1874cc6c1ae77e11
gisehweb <gisehbc@gmail.com>
2022-07-28 18:11:45 -0300

    TP3 punto3 Normalizacion de las URL En el archivo web implementamos las rules y utilizamos el pluralize para que mantenga en singular

--- backend/basic/config/web.php
+++ backend/basic/config/web.php
@@ -51,7 +51,11 @@ $config = [
         'urlManager' => [
             'enablePrettyUrl' => true,
             'showScriptName' => false,
-            'rules' => [],
+            'rules' => [[
+                'class' => 'yii\rest\UrlRule',
+                'controller' => 'apiv1/carrera',
+                'pluralize'=>false,
+            ]],
         ],
         
     ],
