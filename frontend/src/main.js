b5c7014b560577764b0e7fab318b6494daf39d72
gisehweb <gisehbc@gmail.com>
2022-07-29 21:57:16 -0300

    TP4 punto 5 Agregado de la libreria axios se creo el archivo axios.js y se importo en el main.js

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,8 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
 import vuetify from './plugins/vuetify'
+import custom_axios from "@/plugins/axios";
+import VueAxios from "vue-axios";
 
 Vue.config.productionTip = false
+Vue.use(VueAxios, axios, custom_axios);
 
 new Vue({
   vuetify,
---
+++ frontend/src/plugins/axios.js
@@ -0,0 +1,10 @@
+import axios from "axios";
+
+//Config AXIOS
+
+
+axios.defaults.baseURL = "http://127.0.0.1:8000";
+
+const custom_axios = axios;
+
+export default custom_axios; 
