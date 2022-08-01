d4c6e9f164ea6f349c2e475c6b56d416becd5ed7
gisehweb <gisehbc@gmail.com>
2022-08-01 19:11:48 -0300

    TP4 punto 6 Se crearon MainMenu y MainBar, las opciones de menu acercade y home

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import App from './App.vue'
 import vuetify from './plugins/vuetify'
 import custom_axios from "@/plugins/axios";
+import axios from "axios";
 import VueAxios from "vue-axios";
 
 Vue.config.productionTip = false
